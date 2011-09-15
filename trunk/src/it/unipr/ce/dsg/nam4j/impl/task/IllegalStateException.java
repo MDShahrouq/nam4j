4882632fa4ceb8ee23c8e943c6882c8edbc5eddf
michele.amoretti@gmail.com <michele.amoretti@gmail.com@b1ca23d5-8d15-8626-1708-d8b9c3fd3a11>
2011-09-15 17:11:29 +0000

    Tasks!

---
+++ trunk/src/it/unipr/ce/dsg/nam4j/impl/task/IllegalStateException.java
@@ -0,0 +1,14 @@
+package it.unipr.ce.dsg.nam4j.impl.task;
+
+public class IllegalStateException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public IllegalStateException(String errorMsg) {
+		super(errorMsg);
+	}
+
+}
