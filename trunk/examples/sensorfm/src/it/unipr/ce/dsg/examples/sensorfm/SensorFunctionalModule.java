674f04f4b46cf45720ee34a54d5f0ebb3e1c7c1f
michele.amoretti@gmail.com <michele.amoretti@gmail.com@b1ca23d5-8d15-8626-1708-d8b9c3fd3a11>
2011-10-06 19:20:52 +0000

    sensor logger

--- trunk/examples/sensorfm/src/it/unipr/ce/dsg/examples/sensorfm/SensorFunctionalModule.java
+++ trunk/examples/sensorfm/src/it/unipr/ce/dsg/examples/sensorfm/SensorFunctionalModule.java
@@ -5,14 +5,21 @@ import it.unipr.ce.dsg.nam4j.impl.NetworkedAutonomicMachine;
 
 
 public class SensorFunctionalModule extends FunctionalModule {
+	
+	private SensorLogger sLogger = null;
 
 	public SensorFunctionalModule(NetworkedAutonomicMachine nam) {
 		super(nam);
 		this.setId("sfm");
 		this.setName("SensorFunctionalModule");
-		System.out.println("I am " + this.getId() + " and I own to " + nam.getId());
+		this.sLogger = new SensorLogger("log/");
+		sLogger.log("I am " + this.getId() + " and I own to " + nam.getId());
 	}
 
+	public SensorLogger getLogger() {
+		return sLogger;
+	}
+	
 	public void startTemperatureNotification(String locationName, String temperatureValue) {
 		Thread t = new Thread(new ProvideTemperatureRunnable(this, locationName, temperatureValue), "Provide temperature thread");
 		t.start();
